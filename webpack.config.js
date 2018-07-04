adcb374c736dc191faf4bdb027e4344350984514
Duc Tan Dang <ductan.dang@googlemail.com>
2018-07-04 23:22:13 +0200

    chore(webpack)

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const WebpackCleanupPlugin = require('webpack-cleanup-plugin');
+
+module.exports = {
+    entry: './web/src/main.js',
+    output: {
+        path: path.resolve(__dirname, 'web/dist'),
+        filename: 'main.js',
+        publicPath: "./web/dist"
+    },
+    mode: "development",
+    watch: true,
+    watchOptions: {
+        ignored: /node_modules/
+    },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                // 'postcss-loader',
+                'sass-loader',
+            ]
+        }]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+          // Options similar to the same options in webpackOptions.output
+          // both options are optional
+          filename: "[name].css",
+          chunkFilename: "[id].css"
+        }),
+        new WebpackCleanupPlugin()
+    ],
+};
