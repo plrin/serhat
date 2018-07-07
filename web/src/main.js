ce56af94fd4b7eb69a1dcd3b8ef2fa5c7056fd53
Duc Tan Dang <ductan.dang@googlemail.com>
2018-07-07 19:28:00 +0200

    added ajax

--- web/src/main.js
+++ web/src/main.js
@@ -4,3 +4,52 @@ require('./images/blur-city.jpg');
 import './main.scss';
 
 console.log('start app');
+
+
+function getFormData($form) {
+    let formData = new FormData($form);
+    let query = "";
+    for (let pair of formData.entries()) {
+        // console.log(pair[0]+ ', ' + pair[1]);
+        query += pair[0] + "=" + pair[1];
+    }
+
+    return query;
+}
+
+function makeAjaxRequest($form) {
+    
+    let data = getFormData($form);
+    let action = $form.getAttribute('action');
+    let $resultTarget = document.querySelector('[data-result="' + $form.getAttribute('data-result-target') + '"]');
+    console.log(data);
+    let request = new XMLHttpRequest();
+
+    request.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+            $resultTarget.innerHTML = this.responseText;
+        }
+    };
+
+    request.open("GET", action + "?" + data, true);
+    request.send();
+}
+
+
+// bind events
+var addEvent = function($element) {
+    console.log('bind events');
+    $element.addEventListener('submit', (event) =>  {
+        event.preventDefault();
+        console.log(event);
+        makeAjaxRequest(event.target);
+    });
+}
+
+// get forms
+let $forms = document.querySelectorAll('[data-form="search"]');
+for(let i = 0; i < $forms.length; i++) {
+    let $form = $forms[i];
+    addEvent($form);
+}
